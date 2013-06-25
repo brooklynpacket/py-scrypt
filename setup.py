c641d6d2136e6931e7b337249d171ef9b7c5ca15
Christopher Grebs <cg@webshox.org>
2013-06-25 09:31:15 -0700

    Increase package version to correct version.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ scrypt_module = Extension('scrypt',
                           libraries=libraries)
 
 setup(name='scrypt',
-      version='0.5.5',
+      version='1.1.6',
       description='Bindings for the scrypt key derivation function library',
       author='Magnus Hallin',
       author_email='mhallin@gmail.com',
